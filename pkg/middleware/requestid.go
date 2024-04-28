435a53e4652be389ffe24b4cb8e008b0c4116aca
11038569+calamity-m@users.noreply.github.com <11038569+calamity-m@users.noreply.github.com>
2024-04-28 17:57:16 +1000

    Fix request id middleware

--- pkg/middleware/requestid.go
+++ pkg/middleware/requestid.go
@@ -6,14 +6,19 @@ import (
 )
 
 func RequestId(header string) gin.HandlerFunc {
+	mut := true
 
 	return func(ctx *gin.Context) {
 		reqId := ctx.Request.Header.Get(header)
 		if reqId == "" {
 			reqId = uuid.NewString()
+		}
+
+		if mut {
 			ctx.Request.Header.Set(header, reqId)
 		}
 
+		ctx.Header(header, reqId)
 		ctx.Set(header, reqId)
 
 		ctx.Next()
