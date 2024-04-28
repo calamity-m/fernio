dcdeb22405e7d52e3326de41c443221d4771c783
11038569+calamity-m@users.noreply.github.com <11038569+calamity-m@users.noreply.github.com>
2024-04-28 18:00:36 +1000

    Modify how mut variable is used

--- pkg/middleware/requestid.go
+++ pkg/middleware/requestid.go
@@ -5,17 +5,18 @@ import (
 	"github.com/google/uuid"
 )
 
-func RequestId(header string) gin.HandlerFunc {
-	mut := true
+// Grabs the request id header from incoming request, then appends it to the gin context and response headers.
+// If the header is not present or empty, a UUID formatted id will be generated and used instead.
+// If mut is true any newly generaded request id will appended to the incoming request
+func RequestId(header string, mut bool) gin.HandlerFunc {
 
 	return func(ctx *gin.Context) {
 		reqId := ctx.Request.Header.Get(header)
 		if reqId == "" {
 			reqId = uuid.NewString()
-		}
-
-		if mut {
-			ctx.Request.Header.Set(header, reqId)
+			if mut {
+				ctx.Request.Header.Set(header, reqId)
+			}
 		}
 
 		ctx.Header(header, reqId)
